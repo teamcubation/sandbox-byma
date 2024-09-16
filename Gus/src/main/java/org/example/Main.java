d31661d5f30beda8162e8ff59705d893aca367df
TEC - Heredia Gustavo Michel <gustavo.mph@gmail.com>
2024-09-16 10:35:59 -0300

    Testeando la nueva rama-gus

--- Gus/src/main/java/org/example/Main.java
+++ Gus/src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
         System.out.println("Hello world!");
 
-        System.out.println("Hola soy gus");
+        System.out.println("Hola soy gus estoy en mi rama!!!");
     }
 }
