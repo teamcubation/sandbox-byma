a35e2ac3969d55bf0a045d220fab064e76107a8c
Heber Tescione <hebertescione@hotmail.com>
2024-10-01 00:57:23 -0300

    tengo en cuenta principio SOLID open closed y tengo como atributo a la interfaz BonoService agrego anotacion AllArgsContructor en mi controller de bonos

--- Heber/src/main/java/springApp/java/com/example/gestoralyc/controllers/BonoController.java
+++ Heber/src/main/java/springApp/java/com/example/gestoralyc/controllers/BonoController.java
@@ -1,17 +1,16 @@
 package springApp.java.com.example.gestoralyc.controllers;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springApp.java.com.example.gestoralyc.dto.BonoDTO;
-import springApp.java.com.example.gestoralyc.dto.InstrumentoDTO;
 import springApp.java.com.example.gestoralyc.exceptions.InstrumentoDuplicadoException;
 import springApp.java.com.example.gestoralyc.exceptions.InvalidInstrumentoDataException;
 import springApp.java.com.example.gestoralyc.mappers.BonoMapper;
 import springApp.java.com.example.gestoralyc.models.BonoModel;
-import springApp.java.com.example.gestoralyc.services.impl.BonoServiceImpl;
+import springApp.java.com.example.gestoralyc.services.BonoService;
 import springApp.java.com.example.gestoralyc.utils.GeneradorCurl;
 
 import java.util.List;
@@ -19,10 +18,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/bonos") // Este es el path base del controller
 @Slf4j
+@AllArgsConstructor
 public class BonoController
 {
-    @Autowired
-    BonoServiceImpl bonoService;
+    private final BonoService bonoService;
 
     @PostMapping("/")
     public ResponseEntity<BonoDTO> agregarBono(@RequestBody BonoDTO bonoDTO) throws InstrumentoDuplicadoException, InvalidInstrumentoDataException {
