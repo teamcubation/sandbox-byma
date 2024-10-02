172b1f72b37793bbf6ce51f8e9dc4fd82e0e7580
Lucia-azd <azd.lucia@gmail.com>
2024-10-02 16:20:01 -0300

    hago constantes a los msj de excepciones

--- Lu/src/main/java/springBootProject/java/com/example/proyectoSpringBoot/service/serviceImpl/InstrumentoFinancieroServiceImpl.java
+++ Lu/src/main/java/springBootProject/java/com/example/proyectoSpringBoot/service/serviceImpl/InstrumentoFinancieroServiceImpl.java
@@ -16,6 +16,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static com.example.proyectoSpringBoot.utils.ExcepcionesMensajes.*;
+
 
 @Service
 public class InstrumentoFinancieroServiceImpl implements InstrumentoFinancieroServiceInterface {
@@ -31,7 +33,7 @@ public class InstrumentoFinancieroServiceImpl implements InstrumentoFinancieroSe
         Optional<InstrumentoFinanciero> instrumentoFinanciero = this.instrumentoFinancieroRepository.findById(id);
 
         if (instrumentoFinanciero.isEmpty()) {
-            throw new InstrumentoNoEncontradoException("El instrumento financiero a consultar no existe.");
+            throw new InstrumentoNoEncontradoException(MSJ_ELEMENTO_NO_ENCONTRADO);
         }
 
         return InstrumentoFinancieroMapper.toDTO(instrumentoFinanciero.get());
@@ -39,7 +41,7 @@ public class InstrumentoFinancieroServiceImpl implements InstrumentoFinancieroSe
 
     public InstrumentoFinancieroDTO registrar(InstrumentoFinancieroDTO instrumentoFinancieroDTO) throws InstrumentoDuplicadoException, OpcionInvalidaException {
         if(existeNombreInstrumentoFinanciero(instrumentoFinancieroDTO.getNombre())) {
-            throw new InstrumentoDuplicadoException("El instrumento financiero a registrar con ese nombre ya existe.");
+            throw new InstrumentoDuplicadoException(MSJ_ELEMENTO_DUPLICADO);
         }
 
         validarDatos(instrumentoFinancieroDTO);
@@ -55,29 +57,36 @@ public class InstrumentoFinancieroServiceImpl implements InstrumentoFinancieroSe
     public InstrumentoFinancieroDTO editar(Long id, InstrumentoFinancieroDTO instrumentoFinancieroDTO) throws OpcionInvalidaException, InstrumentoNoEncontradoException {
         //validarDatos(instrumentoFinancieroDTO);
 
+        //hacer el if en el caso de que no exista el instrumento financiero
+        Optional<InstrumentoFinanciero> instrumentoFinancieroAEditar = this.instrumentoFinancieroRepository.findById(id);
+
+        if (instrumentoFinancieroAEditar.isEmpty()) {
+            throw new InstrumentoNoEncontradoException(MSJ_ELEMENTO_NO_ENCONTRADO);
+        }
+
         if (existeInstrumentoFinanciero(id)) {
             InstrumentoFinanciero instrumentoFinancieroModel = InstrumentoFinancieroMapper.toModel(instrumentoFinancieroDTO);
             instrumentoFinancieroModel.setId(id);
             return InstrumentoFinancieroMapper.toDTO(this.instrumentoFinancieroRepository.save(instrumentoFinancieroModel));
         }
-        throw new InstrumentoNoEncontradoException("El instrumento financiero a editar no existe");
+        throw new InstrumentoNoEncontradoException(MSJ_ELEMENTO_NO_ENCONTRADO);
     }
 
     private void validarNombre(String nombreAValidar) throws OpcionInvalidaException {
         if (!Validaciones.esNombreValido(nombreAValidar)) {
-            throw new OpcionInvalidaException("Por favor ingrese un nombre valido");
+            throw new OpcionInvalidaException(MSJ_OPCION_INVALIDA);
         }
     }
 
     private void validarPrecio(Double precioAValidar) throws OpcionInvalidaException {
         if (!Validaciones.esPrecioValido(precioAValidar)) {
-            throw new OpcionInvalidaException("Por favor ingrese un precio valido.");
+            throw new OpcionInvalidaException(MSJ_OPCION_INVALIDA);
         }
     }
 
     private void validarTipo(Integer tipoAValidar) throws OpcionInvalidaException {
         if (!Validaciones.esTipoValido(tipoAValidar)) {
-            throw new OpcionInvalidaException("Por favor ingrese un instrumento financieron valido.");
+            throw new OpcionInvalidaException(MSJ_OPCION_INVALIDA);
         }
     }
 
---
+++ Lu/src/main/java/springBootProject/java/com/example/proyectoSpringBoot/utils/ExcepcionesMensajes.java
@@ -0,0 +1,7 @@
+package com.example.proyectoSpringBoot.utils;
+
+public class ExcepcionesMensajes {
+    public static final String MSJ_ELEMENTO_NO_ENCONTRADO = "Instrumento Financiero no encotrado";
+    public static final String MSJ_ELEMENTO_DUPLICADO = "Instrumento Financiero duplicado - No podes tener instrumentos duplicados";
+    public static final String MSJ_OPCION_INVALIDA = "Datos invalidos. Por favor coloque un dato valido";
+}
