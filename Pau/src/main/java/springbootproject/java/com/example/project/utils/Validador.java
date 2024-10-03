95e18506b77138bcfeb74015dee460c1916cdb3a
MariaPaulaBaldivia <m.paulabaldivia00@gmail.com>
2024-10-03 17:04:35 -0300

    Agrego metodos para validar inputs

--- Pau/src/main/java/springbootproject/java/com/example/project/utils/Validador.java
+++ Pau/src/main/java/springbootproject/java/com/example/project/utils/Validador.java
@@ -1,13 +1,15 @@
 package com.example.project.utils;
 
+import java.time.LocalDate;
+
 public class Validador {
-    public static void validarNumeroPositivo(Double unNumero) {
+    public static void validarDoublePositivo(Double unNumero) {
         if(unNumero <= 0) {
             throw new IllegalArgumentException("El numero ingresado no puede ser menor que 0.");
         }
     }
 
-    public static void validarNumeroNoSeaNull(Double unNumero) {
+    public static void validarDoubleNoSeaNull(Double unNumero) {
         if(unNumero == null) {
             throw new IllegalArgumentException("El numero no puede ser nulo.");
         }
@@ -17,8 +19,32 @@ public class Validador {
         if (unString.isEmpty()) {
             throw new IllegalArgumentException("El nombre no puede estar vacio.");
         }
-        if (unString.matches("[0-9]*")) {
+        if (!unString.matches("[A-Za-zÁÉÍÓÚáéíóúÑñ\\s]+")) {
             throw new IllegalArgumentException("El nombre no puede contener numeros.");
+        }
+    }
+
+    public static void validarFechaNoSeaNull(LocalDate unaFecha) {
+        if (unaFecha == null) {
+            throw new IllegalArgumentException("La fecha de nacimiento no puede estar vacia.");
+        }
+    }
+
+    public static void validarNombreAccion(String unNombre) {
+        if (unNombre.isEmpty()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacio.");
+        }
+        if(!unNombre.matches("[A-Z]{3,4}(\\d)?")) {
+            throw new IllegalArgumentException("El nombre ingresado no es valido para una accion.");
+        }
+    }
+
+    public static void validarNombreBono(String unNombre) {
+        if (unNombre.isEmpty()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacio.");
+        }
+        if(!unNombre.matches("[A-Z]{2,6}\\d{2,4}")) {
+            throw new IllegalArgumentException("El nombre ingresado no es valido para una accion.");
         }
     }
 }
